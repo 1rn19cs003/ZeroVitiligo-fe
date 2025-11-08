55d1ff08812f2d7fe5d9b8f06750448e518adf52
Abhishek Jaiswal <abhicse003@gmai.com>
2025-11-08 19:45:18 +0530

    feature:added vistor counter

---
+++ components/Visitor/index.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+export default function Visitor() {
+  const [visitorCount, setVisitorCount] = useState(0);
+  const URL = process.env.NEXT_PUBLIC_SERVER_URL_WEB_SOCKET
+  useEffect(() => {
+    const socket = new WebSocket(URL);
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (data.visitorCount !== undefined) {
+        setVisitorCount(data.visitorCount);
+      }
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  return (
+    <div>
+      <h3>Live Visitors: {visitorCount}</h3>
+    </div>
+  );
+}
