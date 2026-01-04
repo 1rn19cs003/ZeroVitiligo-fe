90fd3c890ae656c0a54ac0771702a09a9f693d40
Abhishek Jaiswal <abhicse003@gmai.com>
2026-01-04 18:42:51 +0530

    added the changes of utils

--- hooks/axios.config.js
+++ hooks/axios.config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { refreshToken } from "../utils/tokenRefresh";
+import { refreshToken } from "../utils/tokenRefresh.js";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
